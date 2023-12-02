1ce44645fd0dc07e4c55825f4360c1831ff6ab2d
Roman Makeev <makeevrserg@gmail.com>
2023-12-02 13:55:43 +0300

    #6 added mockito

--- plugin/src/test/java/com/darkyen/minecraft/SoulDatabaseTest.java
+++ plugin/src/test/java/com/darkyen/minecraft/SoulDatabaseTest.java
@@ -1,27 +1,39 @@
 package com.darkyen.minecraft;
 
-import com.darkyen.minecraft.models.Soul;
 import com.darkyen.minecraft.database.SoulDatabase;
-import com.darkyen.minecraft.mock.ServerStub;
+import com.darkyen.minecraft.models.Soul;
 import com.darkyen.minecraft.utils.channels.ByteBufferChannel;
 import com.darkyen.minecraft.utils.channels.DataInputChannel;
 import com.darkyen.minecraft.utils.channels.DataOutputChannel;
 import com.darkyen.minecraft.utils.serialization.Serialization;
-import org.bukkit.Bukkit;
-import org.bukkit.Material;
-import org.bukkit.inventory.ItemStack;
-import org.jetbrains.annotations.NotNull;
-
 import java.io.IOException;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.Map;
+import java.util.Objects;
 import java.util.UUID;
+import java.util.logging.Logger;
+import org.bukkit.Bukkit;
+import org.bukkit.Material;
+import org.bukkit.Server;
+import org.bukkit.UnsafeValues;
+import org.bukkit.inventory.ItemFactory;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 import org.junit.After;
 import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 import org.junit.Before;
 import org.junit.Test;
+import static org.mockito.Mockito.any;
+import static org.mockito.Mockito.anyInt;
+import static org.mockito.Mockito.anyString;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+import org.mockito.stubbing.Answer;
 
 
 /**
@@ -31,17 +43,69 @@ public final class SoulDatabaseTest {
     //	private static ServerMock server;
     private static DeadSouls plugin;
 
+    UnsafeValues createMockUnsafeValues() {
+        UnsafeValues unsafeValues = mock(UnsafeValues.class);
+        when(unsafeValues.getDataVersion()).thenReturn(1234);
+        when(unsafeValues.getFeatureFlag(any())).thenReturn(null);
+        when(unsafeValues.isSupportedApiVersion(anyString())).thenReturn(false);
+        when(unsafeValues.getMaterial(any(), anyInt())).thenAnswer(
+                (Answer) invocation -> {
+                    Object[] arguments = invocation.getArguments();
+                    if (arguments == null) return null;
+                    if (arguments[0] == null) return null;
+                    else return Material.getMaterial((String) arguments[0]);
+                }
+        );
+        return unsafeValues;
+    }
+
+    ItemFactory createMockItemFactory() {
+        ItemFactory itemFactory = mock(ItemFactory.class);
+        when(itemFactory.getItemMeta(any())).thenReturn(null);
+        when(itemFactory.equals(any(), any())).thenAnswer(
+                (Answer) invocation -> {
+                    Object[] arguments = invocation.getArguments();
+                    @Nullable ItemMeta meta1 = (ItemMeta) arguments[0];
+                    @Nullable ItemMeta meta2 = (ItemMeta) arguments[1];
+                    return Objects.equals(meta1, meta2);
+                }
+        );
+
+        when(itemFactory.ensureServerConversions(any())).thenAnswer(
+                (Answer) invocation -> {
+                    return invocation.getArgument(0);
+                }
+        );
+        return itemFactory;
+    }
+
+    Server createMockServer() {
+        Server server = mock(Server.class);
+        when(server.getLogger()).thenReturn(Logger.getLogger("ServerStub"));
+        when(server.getName()).thenReturn("TEST");
+        when(server.getVersion()).thenReturn("TEST_VER");
+        when(server.getBukkitVersion()).thenReturn("BUKKIT_TEST_VER");
+        when(server.getOnlinePlayers()).thenReturn(Collections.emptyList());
+        when(server.getWhitelistedPlayers()).thenReturn(Collections.emptySet());
+        when(server.getIp()).thenReturn("0.0.0.0");
+        when(server.getUpdateFolder()).thenReturn("server-stub-update-folder");
+        when(server.createExplorerMap(any(), any(), any())).thenReturn(null);
+
+        UnsafeValues unsafeValues = createMockUnsafeValues();
+        when(server.getUnsafe()).thenReturn(unsafeValues);
+
+        ItemFactory itemFactory = createMockItemFactory();
+        when(server.getItemFactory()).thenReturn(itemFactory);
+        return server;
+    }
+
     @Before
     public void setup() {
-//		server = MockBukkit.mock();
-//		plugin = MockBukkit.load(DeadSouls.class);
-        Bukkit.setServer(new ServerStub() {
-        });
+        Bukkit.setServer(createMockServer());
     }
 
     @After
     public void tearDown() {
-//		MockBukkit.unmock();
     }
 
     @Test
